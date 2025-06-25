7c07ed3f85b92756c6e5f85b5af19647756aa5f0
Rogerskelamen <rogerskelamen@gmail.com>
2025-06-24 20:09:47 -0700

    refactor: use singlon class to replace data storage in config.py

--- src/config.py
+++ src/config.py
@@ -1,7 +1,43 @@
-algo_name: str = None
-desc_file: str = None
-src_file: str = None
-head_file: str = None
-tb_file: str = None
-desc_file: str = None
-hls_src: str = None
+class DataConfig:
+    _instance = None
+    _initialized = False
+
+    # real attr
+    algo_name: str = None
+    src_file: str = None
+    head_file: str = None
+    tb_file: str = None
+    desc_file: str = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self,
+                 algo_name: str = None,
+                 src_file: str = None,
+                 head_file: str = None,
+                 tb_file: str = None,
+                 desc_file: str = None) -> None:
+        if self.__class__._initialized:
+            return
+
+        # initialize config instance
+        self.algo_name = algo_name
+        self.src_file = src_file
+        self.head_file = head_file
+        self.tb_file = tb_file
+        self.desc_file = desc_file
+        self.__class__._initialized = True
+
+
+    @classmethod
+    def get_instance(cls):
+        if not cls._instance:
+            raise Exception("DataSetConfig has not been initialized yet.")
+        return cls._instance
+
+
+    def get_loop_file(self):
+        return self.algo_name + "_loop.cpp"
